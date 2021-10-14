d2c557ede93ea88edf9bd71e8f3bb1852b1cec71
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2021-10-14 22:44:17 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["MailCore2"]),
     ],
     targets: [
-        .binaryTarget(name: "MailCore2",
+        .binaryTarget(name: "MailCore",
                       url: "https://github.com/FlowCrypt/mailcore2/releases/download/0.7.0/MailCore.xcframework.zip",
                       checksum: "216784ee811876955afadee7e59b53e233f42dc6571ded7b71a96ad0895f21c6")
     ]
