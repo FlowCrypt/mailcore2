61a41e7a9d77bca62fbf49cd23b16520a4343dcc
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2021-10-14 22:24:50 +0300

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MailCore2",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "MailCore2",
+            targets: ["MailCore2"]),
+    ],
+    targets: [
+        .binaryTarget(name: "MailCore2",
+                      url: "https://github.com/FlowCrypt/mailcore2/releases/download/0.7.0/MailCore.xcframework.zip",
+                      checksum: "216784ee811876955afadee7e59b53e233f42dc6571ded7b71a96ad0895f21c6")
+    ]
+)
