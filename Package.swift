9c8f17dde1e12de0cc57e0bba8e023b1e9d2bd5b
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2021-10-14 22:49:06 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "MailCore2",
-            targets: ["MailCore2"]),
+            targets: ["MailCore"]),
     ],
     targets: [
         .binaryTarget(name: "MailCore",
